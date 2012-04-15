7157cb7f2e98586d1e3f06db795bec1b750bdfac
Andrey Soroka <rokko.noname@gmail.com>
2012-04-15 13:53:05 +0300

    user and bot table center alignment

--- db/schema.rb
+++ db/schema.rb
@@ -33,11 +33,11 @@ ActiveRecord::Schema.define(:version => 20120414151850) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",                            :null => false
-    t.datetime "updated_at",                            :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.string   "encrypted_password"
     t.string   "salt"
-    t.boolean  "admin",              :default => false
+    t.boolean  "admin", :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
